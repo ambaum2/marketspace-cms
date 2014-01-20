e4297a192d78e3f520282d0b65e0ab10a7285ccc
alan baum <ambaum2@gmail.com>
2014-01-20 00:03:04 -0500

    MCD-207 fix product categories so it doesn't look as bad. remove edit profile image upload. remove file browswer from menu

--- sites/all/themes/cm/template.php
+++ sites/all/themes/cm/template.php
@@ -4,3 +4,20 @@
  * @file
  * template.php
  */
+
+function cm_process_page(&$vars, $hooks){
+  //krumo($vars);
+  $excluded_menu_items = array('File browser');
+  for($i=0; $i < count($vars['tabs']['#primary']); $i++) {
+    if($vars['tabs']['#primary'][$i]['#link']['title'] == 'File browser') {
+      unset($vars['tabs']['#primary'][$i]);
+    }
+  }
+}
+
+function cm_form_alter(&$form, $form_state, $form_id) {
+  if($form_id == 'user_profile_form') {
+    //echo "<pre>" . print_r($form, true) . "</pre>";
+    unset($form['picture']);
+  }
+}
