3681c3ef24ce8a84814e77d0db8315cb41bbb550
alan baum <ambaum2@gmail.com>
2014-05-18 11:47:08 -0400

    finish manage products display and setup. MCD-376

--- sites/all/themes/cm/template.php
+++ sites/all/themes/cm/template.php
@@ -5,21 +5,32 @@
  * template.php
  */
 
+/**
+ * @param $vars
+ * @param $hooks
+ */
 function cm_process_page(&$vars, $hooks){
-  //krumo($vars);
-  $excluded_menu_items = array('File browser');
-  for($i=0; $i < count($vars['tabs']['#primary']); $i++) {
-    if(isset($vars['tabs']['#primary'][$i])) {
-      if($vars['tabs']['#primary'][$i]['#link']['title'] == 'File browser') {
-        unset($vars['tabs']['#primary'][$i]);
-      }
+    if (in_array(arg(1), array('manage-products'))) {
+        // name will be "manage-products.tpl.php".
+        $vars['theme_hook_suggestions'][] = '' . arg(1);
     }
-  }
+    $excluded_menu_items = array('File browser');
+    for($i=0; $i < count($vars['tabs']['#primary']); $i++) {
+        if(isset($vars['tabs']['#primary'][$i])) {
+            if($vars['tabs']['#primary'][$i]['#link']['title'] == 'File browser') {
+                unset($vars['tabs']['#primary'][$i]);
+            }
+        }
+    }
 }
 
+/**
+ * @param $form
+ * @param $form_state
+ * @param $form_id
+ */
 function cm_form_alter(&$form, $form_state, $form_id) {
-  if($form_id == 'user_profile_form') {
-    //echo "<pre>" . print_r($form, true) . "</pre>";
-    unset($form['picture']);
-  }
+    if($form_id == 'user_profile_form') {
+        unset($form['picture']);
+    }
 }
