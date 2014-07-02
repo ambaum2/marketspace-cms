fba3efff6e4d397087c8dc1a65b404cd32443c68
alan baum <ambaum2@gmail.com>
2014-07-02 10:26:51 -0400

    add email module. add register local seller and send register email

---
+++ sites/all/modules/custom/ms_email/tests/Ms_Email_UserTest.php
@@ -0,0 +1,33 @@
+<?php
+require 'PHPUnit/Autoload.php';
+class Ms_Email_UserTest extends PHPUnit_Framework_TestCase
+{
+    public $dpl_dir = '/var/www/mspacecms/';
+    const DRUPAL_ROOT = '/var/www/mspacecms/';
+    const REMOTE_ADDR = '162.243.110.111';
+    public $api;
+    public function __construct() {
+        $_SERVER['HTTP_HOST'] = basename($this->dpl_dir);
+        $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+        define('DRUPAL_ROOT', $this->dpl_dir);
+        set_include_path($this->dpl_dir . PATH_SEPARATOR . get_include_path());
+        require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+        drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+        $this->api = new MS_CRM_Api();
+    }
+    public function testSendEmail() {
+//        $email = new MS_User_Email("ambaum2@gmail.com");
+//        $email->passwordResetUrl = "dev.cms.communitymarketspace.com";
+//        $email->tempPassword = "1234";
+//        $email->sendRegistrationEmail();
+    }
+    public function testUserNameExists() {
+        $email = new MS_User_Email("ambaum2@gmail.com");
+        print_r($email->UserNameExists());
+    }
+    public function testNewLocalSellerRegistration() {
+        $email = new MS_User_Email("ambaum2@gmail.com");
+
+        $email->RegisterLocalSellerUser();
+    }
+}
